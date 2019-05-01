dccb0098f49ec68d2e8e8f4fa417e9b22551b4d5
Yonnick <yonnick00@gmail.com>
2019-05-01 07:48:04 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,11 +1,10 @@
 class CreateShows < ActiveRecord::Migration[5.2]
   def change
-    create_table :movies do |t|
-      t.string :title
-      t.integer :release_date
-      t.string :director
-      t.string :lead
-      t.boolean :in_theaters
+    create_table :shows do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
     end
   end
 end
